e1d45f69c3d93c414b2f64f5825cd0bad405ddf8
alimosaed <alireza.parvizimosaed@solace.com>
2024-11-14 10:58:42 -0500

    feat: add json log format

--- src/solace_ai_connector/common/log.py
+++ src/solace_ai_connector/common/log.py
@@ -3,13 +3,25 @@
 import sys
 import logging
 import logging.handlers
+import json
 
 
 log = logging.getLogger("solace_ai_connector")
 
 
+class JsonFormatter(logging.Formatter):
+
+    def format(self, record):
+        log_record = {
+            "time": self.formatTime(record, self.datefmt),
+            "level": record.levelname,
+            "message": record.getMessage(),
+        }
+        return json.dumps(log_record)
+
+
 # Function to setup the configuration for the logger
-def setup_log(logFilePath, stdOutLogLevel, fileLogLevel):
+def setup_log(logFilePath, stdOutLogLevel, fileLogLevel, logFormat):
     # Set the global logger level to the lowest of the two levels
     log.setLevel(min(stdOutLogLevel, fileLogLevel))
 
@@ -20,12 +32,15 @@ def setup_log(logFilePath, stdOutLogLevel, fileLogLevel):
 
     # Create an empty file at logFilePath (this will overwrite any existing content)
     with open(logFilePath, "w") as file:
-        file.write("")        
+        file.write("")
 
     # file_handler = logging.handlers.TimedRotatingFileHandler(
     #    filename=logFilePath, when='midnight', backupCount=30, mode='w')
     file_handler = logging.FileHandler(filename=logFilePath, mode="a")
-    file_formatter = logging.Formatter("%(asctime)s |  %(levelname)s: %(message)s")
+    if logFormat == "json":
+        file_formatter = JsonFormatter()
+    else:
+        file_formatter = logging.Formatter("%(asctime)s |  %(levelname)s: %(message)s")
     file_handler.setFormatter(file_formatter)
     file_handler.setLevel(fileLogLevel)
 
