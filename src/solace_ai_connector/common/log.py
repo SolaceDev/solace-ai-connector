796fd67e3a156c7f6389cfe7c35932a8b10ad210
alimosaed <alireza.parvizimosaed@solace.com>
2024-12-04 12:42:44 -0500

    fix: resolved logging issues

--- src/solace_ai_connector/common/log.py
+++ src/solace_ai_connector/common/log.py
@@ -46,24 +46,6 @@ def convert_to_bytes(size_str):
     return int(size_str)
 
 
-def check_total_size(total_size_cap):
-    total_size = sum(
-        os.path.getsize(handler.baseFilename)
-        for handler in log.handlers
-        if isinstance(handler, logging.handlers.RotatingFileHandler)
-    )
-    if total_size > total_size_cap:
-        return False
-    return True
-
-
-def archive_log_file(logFilePath, log_file_name, max_file_size):
-    if os.path.getsize(logFilePath) > max_file_size:
-        os.rename(logFilePath, log_file_name)
-        with open(logFilePath, "w") as file:
-            file.write("")
-
-
 def setup_log(
     logFilePath,
     stdOutLogLevel,
@@ -90,11 +72,11 @@ def setup_log(
     stream_formatter = logging.Formatter("%(message)s")
     stream_handler.setFormatter(stream_formatter)
 
-    # Create an empty file at logFilePath (this will overwrite any existing content)
-    with open(logFilePath, "w") as file:
-        file.write("")
+    if logFormat == "jsonl":
+        file_formatter = JsonlFormatter()
+    else:
+        file_formatter = logging.Formatter("%(asctime)s |  %(levelname)s: %(message)s")
 
-    file_handler = logging.FileHandler(filename=logFilePath, mode="a")
     if logBack:
         rollingpolicy = logBack.get("rollingpolicy", {})
         if rollingpolicy:
@@ -129,13 +111,13 @@ def setup_log(
             total_size_cap = convert_to_bytes(total_size_cap)
 
             # Generate the log file name using the pattern
-            log_file_name = file_name_pattern.replace("{LOG_FILE}", logFilePath)
-            log_file_name = log_file_name.replace(
-                "%d{yyyy-MM-dd}", datetime.now().strftime("%Y-%m-%d")
+            log_file_name = (
+                file_name_pattern.replace(
+                    "%d{yyyy-MM-dd}", datetime.now().strftime("%Y-%m-%d")
+                )
+                .replace("%i", "0")
+                .replace("${LOG_FILE}", logFilePath)
             )
-            log_file_name = log_file_name.replace(
-                "%i", "0"
-            )  # Initial index for the log file
 
             # Overwrite the file handler with a rotating file handler
             file_handler = logging.handlers.RotatingFileHandler(
@@ -143,24 +125,11 @@ def setup_log(
                 backupCount=max_history,
                 maxBytes=max_file_size,
             )
-
-    if logFormat == "jsonl":
-        file_formatter = JsonlFormatter()
     else:
-        file_formatter = logging.Formatter("%(asctime)s |  %(levelname)s: %(message)s")
+        file_handler = logging.FileHandler(filename=logFilePath, mode="a")
 
     file_handler.setFormatter(file_formatter)
     file_handler.setLevel(fileLogLevel)
 
     log.addHandler(file_handler)
     log.addHandler(stream_handler)
-
-    # # Ensure total size cap is not exceeded
-    # if total_size_cap > 0:
-    #     log.addFilter(lambda record: check_total_size(total_size_cap))
-
-    # # Archive the log file when it exceeds maxBytes
-    # log.addFilter(
-    #     lambda record: archive_log_file(logFilePath, log_file_name, max_file_size)
-    #     or True
-    # )
