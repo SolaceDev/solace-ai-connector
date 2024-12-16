e712b9b0d8a89c7efd32715cb8bf3286931de63f
alimosaed <alireza.parvizimosaed@solace.com>
2024-12-16 15:28:18 -0500

    feat: add the forever retry

--- src/solace_ai_connector/components/inputs_outputs/broker_base.py
+++ src/solace_ai_connector/components/inputs_outputs/broker_base.py
@@ -30,6 +30,7 @@ from ...common.utils import encode_payload, decode_payload
 
 
 class BrokerBase(ComponentBase):
+
     def __init__(self, module_info, **kwargs):
         super().__init__(module_info, **kwargs)
         self.broker_properties = self.get_broker_properties()
@@ -94,6 +95,10 @@ class BrokerBase(ComponentBase):
             "subscriptions": self.get_config("broker_subscriptions"),
             "trust_store_path": self.get_config("trust_store_path"),
             "temporary_queue": self.get_config("temporary_queue"),
+            "reconnection_strategy": self.get_config("reconnection_strategy"),
+            "retry_interval": self.get_config("retry_interval"),
+            "retry_count": self.get_config("retry_count"),
+            "retry_interval": self.get_config("retry_interval"),
         }
         return broker_properties
 
