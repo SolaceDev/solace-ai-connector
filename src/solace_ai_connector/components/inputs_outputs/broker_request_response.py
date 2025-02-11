3e9655592f90471594e2775d051615c51273b9cd
Cyrus Mobini <cyrus.mobini@solace.com>
2025-02-11 17:11:32 -0500

    moved before errors

--- src/solace_ai_connector/components/inputs_outputs/broker_request_response.py
+++ src/solace_ai_connector/components/inputs_outputs/broker_request_response.py
@@ -329,6 +329,8 @@ class BrokerRequestResponse(BrokerBase):
             topic = broker_message.get("topic")
             user_properties = broker_message.get("user_properties", {})
 
+        self.messaging_service.ack_message(broker_message)
+        
         if not user_properties:
             log.error("Received response without user properties: %s", payload)
             return
@@ -419,8 +421,6 @@ class BrokerRequestResponse(BrokerBase):
         if last_piece:
             self.cache_service.remove_data(request_id)
     
-        self.messaging_service.ack_message(broker_message)
-
     def invoke(self, message, data):
         request_id = str(uuid.uuid4())
 
